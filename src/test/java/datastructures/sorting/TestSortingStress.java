433b51b23516626b74d7a6c9b1451014996fdcea
andrew <andrew7011616@gmail.com>
2018-11-06 15:39:39 -0800

    stress test update

--- src/test/java/datastructures/sorting/TestSortingStress.java
+++ src/test/java/datastructures/sorting/TestSortingStress.java
@@ -10,40 +10,37 @@ import datastructures.concrete.DoubleLinkedList;
 import datastructures.interfaces.IList;
 import datastructures.interfaces.IPriorityQueue;
 
-import static org.junit.Assert.assertTrue;
 
 /**
  * See spec for details on what kinds of tests this class should include.
  */
 public class TestSortingStress extends BaseTest {
+    int cap = 5000000;
+    
     @Test(timeout=15*SECOND)
     public void testArrayHeap() {
         IPriorityQueue<Integer> heap = new ArrayHeap<Integer>();
-        for (int i = 200000; i > 0; i--) {
+        for (int i = cap; i > 0; i--) {
             heap.insert(i);
-            assertEquals(200001 - i, heap.size());
+            assertEquals(cap + 1 - i, heap.size());
         }
-        int min = -1;
         for (int i = 0; i < heap.size(); i++) {
-            assertTrue(heap.peekMin() > min);
-            min = heap.removeMin();
-            assertEquals(199999 - i, heap.size());
+            assertEquals(i + 1, heap.removeMin());
+            assertEquals(cap -1 - i, heap.size());
         }
     }
     
     @Test(timeout=15*SECOND)
     public void testKTopSort() {
         IList<Integer> list = new DoubleLinkedList<Integer>();
-        for (int i = 200000; i > 0; i--) {
+        for (int i = 0; i > cap; i--) {
             list.add(i);
-            assertEquals(200001 - i, list.size());
+            assertEquals(i + 1, list.size());
         }
-        IList<Integer> top = Searcher.topKSort(199999, list);
-        int min = -1;
+        IList<Integer> top = Searcher.topKSort(cap - 1, list);
         for (int i = 0; i < top.size(); i++) {
-            assertTrue(top.get(0) > min);
-            min = top.delete(0);
-            assertEquals(199998 - i, top.size());
+            assertEquals(i + 1, top.delete(0));
+            assertEquals(cap - 2 - i, top.size());
         }
     }
 }
