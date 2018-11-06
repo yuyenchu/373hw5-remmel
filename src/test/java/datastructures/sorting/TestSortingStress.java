ae514777748108ca6b2b57f610f21ad074fba80d
andrew <andrew7011616@gmail.com>
2018-11-06 15:29:53 -0800

    stress test update(2 tests added)

--- src/test/java/datastructures/sorting/TestSortingStress.java
+++ src/test/java/datastructures/sorting/TestSortingStress.java
@@ -1,7 +1,14 @@
 package datastructures.sorting;
 
 import misc.BaseTest;
+import misc.Searcher;
+
 import org.junit.Test;
+
+import datastructures.concrete.ArrayHeap;
+import datastructures.concrete.DoubleLinkedList;
+import datastructures.interfaces.IList;
+import datastructures.interfaces.IPriorityQueue;
 
 import static org.junit.Assert.assertTrue;
 
@@ -9,9 +16,34 @@ import static org.junit.Assert.assertTrue;
  * See spec for details on what kinds of tests this class should include.
  */
 public class TestSortingStress extends BaseTest {
-    @Test(timeout=10*SECOND)
-    public void testPlaceholder() {
-        // TODO: replace this placeholder with actual code
-        assertTrue(true);
+    @Test(timeout=15*SECOND)
+    public void testArrayHeap() {
+        IPriorityQueue<Integer> heap = new ArrayHeap<Integer>();
+        for (int i = 200000; i > 0; i--) {
+            heap.insert(i);
+            assertEquals(200001 - i, heap.size());
+        }
+        int min = -1;
+        for (int i = 0; i < heap.size(); i++) {
+            assertTrue(heap.peekMin() > min);
+            min = heap.removeMin();
+            assertEquals(199999 - i, heap.size());
+        }
+    }
+    
+    @Test(timeout=15*SECOND)
+    public void testKTopSort() {
+        IList<Integer> list = new DoubleLinkedList<Integer>();
+        for (int i = 200000; i > 0; i--) {
+            list.add(i);
+            assertEquals(200001 - i, list.size());
+        }
+        IList<Integer> top = Searcher.topKSort(199999, list);
+        int min = -1;
+        for (int i = 0; i < top.size(); i++) {
+            assertTrue(top.get(0) > min);
+            min = top.delete(0);
+            assertEquals(199998 - i, top.size());
+        }
     }
 }
